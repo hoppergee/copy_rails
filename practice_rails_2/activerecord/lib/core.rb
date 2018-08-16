a0bdc9d943feea1547e500f285c13574ca1bb576
吉翔 <jx4554@gmail.com>
2018-08-17 07:56:45 +0800

    p2: ActiveRecord::Core(continue)

--- practice_rails_2/activerecord/lib/core.rb
+++ practice_rails_2/activerecord/lib/core.rb
@@ -11,6 +11,8 @@ module ActiveRecord
     included do
       mattr_accessor :logger, instance_writer: false
 
+      mattr_accessor :verbose_query_logs, instance_writer: false, default: false
+
       def self.configuration=(config)
         @@configurations = ActiveRecord::ConnectionHandling::MergeAndResolveDefaultUrlConfig.new(config).resolve
       end
@@ -157,6 +159,36 @@ module ActiveRecord
         name = attribute_alias(name) if attribute_alias?(name)
         table(name)
       end
+
+      def predicate_builder
+        @predicate_builder ||= PredicateBuilder.new(table_metadata)
+      end
+
+      def type_caster
+        TypeCaster::Map.new(self)
+      end
+
+      private
+
+        def cached_find_by_statement(key, &block)
+          cache = @find_by_statement_cache[connection.prepared_statment]
+          cache.compute_if_absent(key) { StatementCache.create(connection, &block) }
+        end
+
+        def relation
+          relation = Relation.create(self)
+
+          if finder_needs_type_condition? && !ignore_default_scope?
+            relation.where!(type_condition)
+            relation.create_with!(inheritance_column.to_s => sti_name)
+          else
+            relation
+          end
+        end
+
+        def table_metadata
+          TableMetadata.new(self, arel_table)
+        end
     end
 
     def initialize(attributes = nil)
@@ -171,5 +203,157 @@ module ActiveRecord
       yield self if block_given?
       _run_initialize_callbacks
     end
+
+    def init_with(coder)
+      coder = LegacyYamlAdapter.convert(self.class, coder)
+      @attributes = self.class.yaml_encoder.decode(coder)
+
+      init_internals
+
+      @new_record = coder["new_record"]
+
+      self.class.define_attributes_methods
+
+      yield self if block_given?
+
+      _fun_find_callbacks
+      _run_initialize_callbacks
+
+      self
+    end
+
+    def initialize_dup(other)
+      @attributes = @attributes.deep_dup
+      @attributes.reset(self.class.primary_key)
+
+      _run_initialize_callbacks
+
+      @new_record = true
+      @destroyed  = true
+      @_start_transaction_state = {}
+      @transaction_state = nil
+
+      super
+    end
+
+    def encode_with(coder)
+      self.class.yaml_encoder.encode(@attributes, coder)
+      coder["new_record"] = new_record?
+      coder["active_record_yaml_version"] = 2
+    end
+
+    def ==(comparison_object)
+      super ||
+        comparison_object.instance_of?(self.class) &&
+        !id.nil? &&
+        comparison_object.id == id
+    end
+    alias :eql? :==
+
+    def hash
+      if id
+        self.class.hash ^ id.hash
+      else
+        super
+      end
+    end
+
+    def freeze
+      @attributes = @attributes.clone.freeze
+      self
+    end
+
+    def frozen?
+      @attributes.frozen?
+    end
+
+    def <=>(other_object)
+      if other_object.is_a?(self.class)
+        to_key <=> other_object.to_key
+      else
+        super
+      end
+    end
+
+    def readonly?
+      @readonly
+    end
+
+    def readonly!
+      @readonly = true
+    end
+
+    def connection_handler
+      self.class.connection_handler
+    end
+
+    def inspect
+      inspection = if defined?(@attributes) && @attributes
+        self.class.attribute_names.collect do |name|
+          if has_attribute?(name)
+            "#{name}: #{attribute_for_inspect(name)}"
+          end
+        end.compact.join(", ")
+      else
+        "not initialized"
+      end
+
+      "#<#{self.class} #{inspection}>"
+    end
+
+    def pretty_print(pp)
+      return super if custom_inspect_method_defined?
+      pp.object_address_group(self) do
+        if defined?(@attributes) && @attributes
+          column_names = self.class.column_names.select { |name| has_attribute?(name) || new_record? }
+          pp.seplist(column_names, proc { pp.text "," }) do |column_name|
+            column_value = read_attribute(column_name)
+            pp.breakable " "
+            pp.group(1) do
+              pp.text column_name
+              pp.text ":"
+              pp.breakable
+              pp.pp column_value
+            end
+          end
+        else
+          pp.breakable " "
+          pp.text "not initialized"
+        end
+      end
+    end
+
+    def slice(*methods)
+      Hash[methods.flatten.map! { |method| [method, public_send(method)] }].with_indifferent_access
+    end
+
+    private
+
+      def to_ary
+        nil
+      end
+
+      def init_internals
+        @readonly = false
+        @destroyed = false
+        @marked_for_destruction = false
+        @destroyed_by_association = nil
+        @new_record = true
+        @_start_transaction_state = {}
+        @transaction_state = nil
+      end
+
+      def initialize_internals_callback
+      end
+
+      def thaw
+        if fronzen?
+          @attributes = @attributes.dup
+        end
+      end
+
+      def custom_inspect_method_defined?
+        self.class.instance_method(:inspect).owner != ActiveRecord::Base.instance_method(:inspect).owner
+      end
   end
 end
